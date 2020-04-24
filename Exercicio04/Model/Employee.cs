8c180cd2aafbc2645fe5c9e50a31b752e7c1ad79
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2020-04-24 16:27:01 -0300

    Exercicio porcentagem

---
+++ Exercicio04/Model/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio04.Model
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public double Tax { get; set; }
+
+        public double NetSalary()
+        {
+            return Salary - Tax;
+        }
+
+        public double IncreasesSalary(double tax)
+        {
+            double percentual = tax / 100.0;
+            double valor_final = Salary + (percentual * Salary);
+            return valor_final;
+        }
+    }
+}
---
+++ Exercicio04/Program.cs
@@ -0,0 +1,31 @@
+﻿using Exercicio04.Model;
+using System;
+using System.Globalization;
+
+namespace Exercicio04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tax = 0;
+            Employee employee = new Employee();
+
+            Console.WriteLine("Enter name of the Employee: ");
+            employee.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter Salary of the Employee: ");
+            employee.Salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Enter of the tax: ");
+            employee.Tax = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(employee.NetSalary());
+
+            Console.WriteLine("Enter a new percentage rate: ");
+            tax = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(employee.IncreasesSalary(tax));
+        }
+    }
+}
